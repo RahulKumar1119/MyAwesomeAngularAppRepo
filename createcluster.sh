0c5c3f008c4ddf104a376d1aa05046346dd54461
RahulKumar1119 <suratirahulkumar29@gmail.com>
2021-09-02 15:29:33 +0530

    changes in Jenkinsfile

---
+++ createcluster.sh
@@ -0,0 +1,6 @@
+eksctl create cluster --name=eksdemo1 \
+                      --region=ap-south-1 \
+		      --version=1.17 \
+                      --zones=ap-south-1a,ap-south-1b \
+                      --without-nodegroup 
+
---
+++ nodegroup.sh
@@ -0,0 +1,17 @@
+eksctl create nodegroup --cluster=eksdemo1 \
+                        --region=ap-south-1 \
+                        --name=eksdemo1-ng-private1 \
+                        --node-type=t3.small \
+                        --nodes-min=2 \
+                        --nodes-max=3 \
+			--spot \
+                        --node-volume-size=10 \
+                        --ssh-access \
+                        --ssh-public-key=rahul \
+                        --managed \
+                        --asg-access \
+                        --external-dns-access \
+                        --full-ecr-access \
+                        --appmesh-access \
+                        --alb-ingress-access \
+                        --node-private-networking  
